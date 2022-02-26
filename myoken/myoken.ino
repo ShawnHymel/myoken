590e8c45fa456e9a4ace741d4be4538d606848f5
ShawnHymel <5232145+ShawnHymel@users.noreply.github.com>
2022-02-26 16:19:31 -0700

    Finished code. Should be ready to wear.

--- myoken/myoken.ino
+++ myoken/myoken.ino
@@ -1,27 +1,50 @@
 /**
- * Myoken - God of the North Star
+ * Project Myoken (God of the North Star)
+ * 
+ * Wearable that buzzes when user is facing (approximately) north.
+ * 
+ * Author: Shawn Hymel
+ * Date: February 26, 2022
+ * 
+ * Dr. Susan Barry reportedly trained herself to know where north is at all
+ * times: https://www.psychologytoday.com/us/blog/eyes-the-brain/201009/can-you-acquire-sense-direction
+ * 
+ * This project attempts to recreate that experiment.
+ * 
+ * First, find the hard-iron and soft-iron calibration values for your MLX90393
+ * magnetometer as well as the magnetic declination value for your area. Fill out
+ * those settings below.
+ * 
+ * Connect the MLX90393 to the I2C port and connect a vibration motor (through
+ * a transistor for current gain) to a PWM pin (default pin 9). Upload this sketch
+ * and wear the project for 2 weeks, making sure to walk around new places. After
+ * removing it, see if you can predict where north is.
  * 
  * Note: Double-tap reset to put into bootloader mode to upload sketch. Sleep
  * mode disables UART.
+ * 
+ * License: 0BSD (https://opensource.org/licenses/0BSD)
  */
 
 #include "ArduinoLowPower.h"
 #include "Adafruit_MLX90393.h"
 
-#define DEBUG   1
+#define DEBUG 0
 
 // Pins
 const int vib_pin = 9;
 const int led_pin = LED_BUILTIN;
 
 // Settings
-const int sleep_ms = 50;          // Time (ms) to sleep between samples
-const float target_dir = 0.0;     // Target is "North"
-const float target_tol = 10.0;    // +/-10 degrees of "North" is acceptable
+const int sleep_ms = 50;            // Time (ms) to sleep between samples
+const float target_dir = 0.0;       // Target is "North"
+const float target_tol = 10.0;      // +/-10 degrees of "North" is acceptable
+const float hyst_tol = 20.0;        // +/-20 degrees of "North" to re-arm buzzer
+const unsigned long buzz_dur = 500; // Time (ms) that buzzer is on
 
 // Hard-iron calibration settings
 const float hard_iron[3] = {
-  6.43,  -25.08, 7.73
+     6.43,  -25.08,  7.73
 };
 
 // Soft-iron calibration settings
@@ -35,7 +58,7 @@ const float soft_iron[3][3] = {
 // East is positive (+), west is negative (-)
 // mag_decl = (+/-)(deg + min/60 + sec/3600)
 // Set to 0 to get magnetic heading instead of geo heading
-const float mag_decl = 0.0;
+const float mag_decl = 7.92;
 
 // Sensor settings
 #define MLX_GAIN          MLX90393_GAIN_1X
@@ -89,6 +112,7 @@ void loop() {
 
   static float hi_cal[3];
   static float heading;
+  static float facing_north = false;
 
   // Sample and compensate with hard/soft-iron calibration data
   if (mlx.readData(&mag_data[0], &mag_data[1], &mag_data[2])) {
@@ -127,9 +151,22 @@ void loop() {
     // Determine if we're facing about "North"
     if ((heading >= target_dir - target_tol) && 
         (heading <= target_dir + target_tol)) {
-      led_vib(HIGH, 255);
+
+      // Only buzz if we were not previously facing North
+      if (facing_north == false) {
+        facing_north = true;
+        led_vib(HIGH, 255);
+        delay(500);
+        led_vib(LOW, 0);
+      }
+
+    // Use hysteresis: only re-arm the buzzer if we move outside +/-20 deg of N
     } else {
-      led_vib(LOW, 0);
+      if ((heading <= target_dir - hyst_tol) || 
+          (heading >= target_dir + hyst_tol)) {
+        facing_north = false;
+        Serial.println("Re-armed");
+      }
     }
 
   } else {
@@ -147,11 +184,12 @@ void loop() {
   // Go to sleep
 #if DEBUG
   delay(sleep_ms);
-#elif
+#else
   LowPower.sleep(sleep_ms);
 #endif
 }
 
+// Buzz the vibration motor and flash the LED if debugging is enabled
 void led_vib(uint8_t led, uint8_t vib) {
 #if DEBUG
   digitalWrite(led_pin, led);
